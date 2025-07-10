87254a7a831b35f5c6fbaa3153ba62b91d42b1f3
Lucas Dias <93742838+lucasd788@users.noreply.github.com>
2025-07-10 15:25:04 -0300

    Delete ContaCerta_BACK directory

--- ContaCerta_BACK/prisma/migrations/20250516012151_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Fatura" ADD COLUMN     "valorTotal" DOUBLE PRECISION NOT NULL DEFAULT 0;
--- ContaCerta_BACK/src/routes/auth.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { loginController } from '../controllers/auth.controller';
-
-const router = Router();
-router.post('/login', loginController);
-
-export default router;
