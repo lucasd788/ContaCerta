d910839c959c8c4500d7b39c57dd645666004b74
Lucas Dias <93742838+lucasd788@users.noreply.github.com>
2025-07-10 15:25:19 -0300

    Delete ContaCerta_FRONT directory

--- ContaCerta_FRONT/src/routes/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../auth/AuthContext";
-import type { JSX } from "react";
-
-export default function PrivateRoute({ children }: { children: JSX.Element }) {
-  const { token } = useAuth();
-  return token ? children : <Navigate to="/login" />;
-}
